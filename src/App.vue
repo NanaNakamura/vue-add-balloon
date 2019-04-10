9fcc39aa460a619801c68932edeb443f517588ee
nakamura <nakamura@1-10.com>
2019-04-10 13:11:29 +0900

    fc

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>Add Balloon at chat</h1>
+    <AddBalloon/>
+  </div>
+</template>
+
+<script>
+import AddBalloon from './components/AddBalloon'
+export default {
+  name: 'App',
+  components: {
+    AddBalloon
+  }
+}
+</script>
+
+<style lang="scss" module>
+@import "../node_modules/formula-css/develop/formula.scss";
+</style>
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin: 100px 4%;
+  h1 {
+    margin-bottom: 80px;
+    font-size: 2em;
+    font-weight: bold;
+  }
+}
+</style>
